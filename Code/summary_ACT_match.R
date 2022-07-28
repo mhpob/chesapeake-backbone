19627b3af255b50cfd4860faa2bf783015d24c6c
Mike O'Brien <mikeob9@gmail.com>
2022-07-28 10:19:34 -0400

    Add plotting

--- Code/summary_ACT_match.R
+++ Code/summary_ACT_match.R
@@ -3,7 +3,7 @@ library(data.table)
 act <- readxl::read_excel('c:/users/darpa2/downloads/Active transmitters_20210810.xlsx')
 
 dets <- list.files('p:/obrien/biotelemetry/mainstem backbone/detection files',
-                   pattern = '1214.*.csv',
+                   pattern = '*.csv',
                    full.names = T)
 
 
@@ -21,5 +21,21 @@ act_match[, mon := month(dateandtimeutc)]
 act_match[, commonname := tolower(`Common Name`)]
 
 xtabs(~ commonname + mon, data = act_match)
-xtabs(~ commonname + mon, data = unique(act_match, by = 'transmitter'))
+xtabs(~ commonname + mon , data = unique(act_match, by = 'transmitter'))
 unique(act_match$`Primary Researcher`)
+
+library(ggplot2)
+
+ggplot(data = act_match[, .(N = .N), by = c('commonname', 'mon')]) +
+  geom_line(aes(x = mon, y = N)) +
+  labs(y = 'N detections', x = '') +
+  facet_wrap(~ commonname, scales = 'free_y') +
+  scale_x_continuous(breaks = 1:12) +
+  theme_minimal()
+
+ggplot(data = unique(act_match, by = 'transmitter')[, .(N = .N), by = c('commonname', 'mon')]) +
+  geom_line(aes(x = mon, y = N)) +
+  labs(y = 'N fish', x = '') +
+  facet_wrap(~ commonname, scales = 'free_y') +
+  scale_x_continuous(breaks = 1:12) +
+  theme_minimal()
